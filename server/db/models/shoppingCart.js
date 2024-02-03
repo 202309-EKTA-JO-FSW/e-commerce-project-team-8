cda231fa97c935960597ce7546b0cae6246e1ecb
luj00 <145015534+luj00@users.noreply.github.com>
2024-02-03 21:29:14 +0300

    update 3

---
+++ server/db/models/shoppingCart.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const shoppingCartSchema = new mongoose.Schema({
+  customerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer',
+    required: true,
+  },
+  items: [
+    {
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('ShoppingCart', shoppingCartSchema);
